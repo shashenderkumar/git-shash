cdc8caa6d7ee856bec2c5d3262642cc0f6c4fd5b
Shashender Kumar <shashender@gmail.com>
2020-10-04 23:02:23 +0530

    watsapp lib

---
+++ pywhat.py
@@ -0,0 +1,5 @@
+import pywhatkit as pwk 
+#(phone no. with country code , message, time: hours, minutes)
+#pwk.sendwhatmsg("+919891xxxxxx", "Sent with Python", 9, 25)
+#pwk.playonyt("comedy circus")
+pwk.showHistory()
